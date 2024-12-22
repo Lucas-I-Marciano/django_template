4246c00950b3434f831b5de5afdd6d04f60e45c4
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-22 15:53:24 -0300

    :sparkles: feat: Rendering a html page instead of write html

--- galeria/views.py
+++ galeria/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hello World</h1><img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTPsp9j_fEDBRLmPBRlmWgGqdBgZ5CGaVoGPYAlVBsTYA3Iim6:https://e7.pngegg.com/pngimages/148/800/png-clipart-agnes-youtube-despicable-me-animation-youtube-child-toddler-thumbnail.png&s>")
+    return render(request, 'test.html')
