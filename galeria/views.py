86b9694ca6f2269555c2a3c3a7bb0d5535465a95
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-22 15:30:06 -0300

    :sparkles: feat: Creating my first HTML view

--- galeria/views.py
+++ galeria/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Hello World</h1><img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTPsp9j_fEDBRLmPBRlmWgGqdBgZ5CGaVoGPYAlVBsTYA3Iim6:https://e7.pngegg.com/pngimages/148/800/png-clipart-agnes-youtube-despicable-me-animation-youtube-child-toddler-thumbnail.png&s>")
