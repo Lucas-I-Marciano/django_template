d4423bdba1c46901eb555cae03de1a2e27d22a44
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-27 17:37:14 -0300

    :sparkles: feat: Adding 'publicado' field on my model
    It will handle with what data will be displayed

--- galeria/models.py
+++ galeria/models.py
@@ -16,6 +16,7 @@ class Fotografia(models.Model):
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     categoria = models.CharField(max_length=20, choices=CATEGORIAS, null=False, default='')
+    publicado = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Fotografia: [Nome: {self.nome}]'
