f6e2474b6c7a0a631321d04b25bd72dceb049d65
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-27 16:30:17 -0300

    :sparkles: feat: Adding search field by 'nome' field on admin page

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,7 +4,9 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class FotografiaAdmin(admin.ModelAdmin):
-    list_display = ["nome", "legenda", "descricao"]
+    list_display = ["id", "nome", "legenda", "descricao"]
     empty_value_display = "-???-"
+    list_display_links = ["id", "nome"]
+    search_fields = ["nome"]
 
 admin.site.register(Fotografia, FotografiaAdmin)
