46589682ff4c01b8c1c3ffb51ea519a7b53bb2ad
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-27 16:42:13 -0300

    :sparkles: feat: Adding choice field to my 'Fotografia' model
    To do that, I must provide two attributes: choices -> The choices itself, default -> Default value to populate database as it can not be null

--- galeria/models.py
+++ galeria/models.py
@@ -3,10 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Fotografia(models.Model):
+    CATEGORIAS = [
+        #  1st element -> stored in the database. 2nd element -> displayed.
+        ("nebulosa", "Nebulosa"),
+        ("estrela", "Estrela"),
+        ("galaxia", "Galáxia"),
+        ("planeta", "Planeta")
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=100, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=20, choices=CATEGORIAS, null=False, default='')
 
     def __str__(self):
         return f'Fotografia: [Nome: {self.nome}]'
