8e817a21ea16018a3439f473114771cb601cd75b
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-27 17:48:48 -0300

    :sparkles: feat: Adding 'publicado' field on admin page and allowing it to be editable without entering in it

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,11 +4,12 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class FotografiaAdmin(admin.ModelAdmin):
-    list_display = ["id", "nome", "legenda", "descricao"]
+    list_display = ["id", "nome", "legenda", "descricao", "publicado"]
     empty_value_display = "-???-"
     list_display_links = ["id", "nome"]
     search_fields = ["nome"]
     list_filter = ["categoria"]
     list_per_page = 10
+    list_editable = ["publicado"]
 
 admin.site.register(Fotografia, FotografiaAdmin)
