b9471747f44c12731ccbc81fbea1abd67ccb25b2
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-27 14:48:00 -0300

    :sparkles: feat: Sending all data from 'Fotografias' table
    Using manage.py shell I inserted a value on my table importing my model, instancing my class [my_instance = Fotografia(...)] and using my_instance.save()

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from galeria.models import Fotografia
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"fotografias" : fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
