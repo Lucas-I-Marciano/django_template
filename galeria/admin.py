1b1cbddc38c307be7c6c96a1bae30140a91afdd5
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-27 16:15:28 -0300

    :sparkles: feat: Adding 'Fotografia' model to my admin page
    To access admin/ endpoint, I created a super user with 'manage.py createsuperuser' command

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
 # Register your models here.
+
+class FotografiaAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Fotografia, FotografiaAdmin)
