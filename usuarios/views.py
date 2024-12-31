cc61e99916b2d3bd4578ebc8530fbe25cee7339f
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-31 14:55:21 -0300

    :sparkles: feat: Creating intelligence to get data from form if method is 'POST'
    Accessing 'cadastro.html' through link, it will be a GET http verb, while by fullfilling the form, it will access through a 'POST' http verb'

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 from usuarios.forms import LoginForm, CadastroForm
 
@@ -9,5 +11,33 @@ def login(request):
     return render(request, "usuarios/login.html", {"form" : form})
 
 def cadastro(request):
+    print("Executando a função cadastro")
     form = CadastroForm()
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        print("Método é post")
+        # check whether it's valid:
+        form = CadastroForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+
+            if form['senha_1'].value() != form['senha_2'].value() :
+                return redirect('cadastro') 
+            
+            nome = form['nome_login'].value()
+            email = form['email'].value()
+            senha = form['senha_1'].value()
+
+            if User.objects.filter(username=nome).exists() :
+                return redirect('cadastro')
+
+            user = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            user.save()
+            return redirect("login")
+
+            
     return render(request, 'usuarios/cadastro.html', {"form" : form})
