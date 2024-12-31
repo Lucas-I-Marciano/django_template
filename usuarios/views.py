2b93a23a073ef0227411e15cc62298b2d243a537
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-30 21:54:00 -0300

    :sparkles: feat: Importing login form on its view

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from usuarios.forms import LoginForm
+
 # Create your views here.
 
 def login(request):
-    return render(request, "usuarios/login.html")
+    form = LoginForm()
+    return render(request, "usuarios/login.html", {"form" : form})
 
 def cadastro(request):
     return render(request, 'usuarios/cadastro.html')
