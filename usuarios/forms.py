dc5c0089280e3b4bdae3a765f86ceffb6cac81dc
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-30 22:19:51 -0300

    :sparkles: feat: Adding widget and its attributes on each field of my form

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,9 +3,25 @@ from django import forms
 
 class LoginForm(forms.Form):
     nome_login = forms.CharField(
-        label="Nome de Login"
+        label="Nome de Login",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class" : "form-control",
+                "placeholder": "Ex.: João Silva",
+            }
+        )
     )
     senha = forms.CharField(
-        label="Senha"
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class" : "form-control",
+                "placeholder": "Digite sua senha",
+            }
+        )
     )
     
