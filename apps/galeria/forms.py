9dea390ec422c3acbe38d5013b306ec59faf9974
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-06 12:27:22 -0300

    :sparkles: feat: Adding widgets on my forms
    It will now have style because of class and specifying date type

--- apps/galeria/forms.py
+++ apps/galeria/forms.py
@@ -1,7 +1,24 @@
-from django.forms import ModelForm
+from django import forms
 from apps.galeria.models import Fotografia
 
-class FotografiaForm(ModelForm):
+class FotografiaForm(forms.ModelForm):
     class Meta:
         model = Fotografia
         exclude = ['publicado']
+
+        widgets = {
+            "nome" : forms.TextInput(attrs={"class":"form-control"}) ,
+            "legenda" : forms.TextInput(attrs={"class":"form-control"}),
+            "descricao" : forms.Textarea(attrs={"class":"form-control"}),
+            "foto" : forms.FileInput(attrs={"class":"form-control"}),
+            "categoria" : forms.Select(attrs={"class":"form-control"}),
+            "data_fotografia" : forms.DateInput(
+                format = "%Y/%m/%d",
+                attrs={
+                    "class":"form-control",
+                    "placeholder": "YYYY/MM/DD",
+                    "type":"date"
+                    }
+                ),
+            "usuario" : forms.Select(attrs={"class":"form-control"})
+        }
