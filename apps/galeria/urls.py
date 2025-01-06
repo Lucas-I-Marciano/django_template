114812fcfb77ec3c7c6fa97ac4d1ffdbe8d6600a
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-06 17:13:12 -0300

    :sparkles: feat: Creating view function to delete picture based on its ID

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.galeria.views import index, imagem, busca, adicionar_imagem, editar_imagem
+from apps.galeria.views import index, imagem, busca, adicionar_imagem, editar_imagem, excluir_imagem
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("busca", busca, name="busca"),
     path("adicionar-imagem", adicionar_imagem, name="adicionar_imagem"),
     path("editar-imagem/<int:foto_id>", editar_imagem, name="editar_imagem"),
+    path("excluir-imagem/<int:foto_id>", excluir_imagem, name="excluir_imagem"),
 ]
