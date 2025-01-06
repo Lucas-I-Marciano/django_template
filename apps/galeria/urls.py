2f5bccf7ae7aa001db500f76e4c3478e89f25b32
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2025-01-06 13:16:09 -0300

    :sparkles: feat: Creating my url to edit pictures
    It will expect foto_id to track picture

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.galeria.views import index, imagem, busca, adicionar_imagem
+from apps.galeria.views import index, imagem, busca, adicionar_imagem, editar_imagem
 
 urlpatterns = [
     path("", index, name="index"),
     path("imagem/<int:foto_id>", imagem, name="imagem"),
     path("busca", busca, name="busca"),
-    path("adicionar-imagem", adicionar_imagem, name="adicionar_imagem")
+    path("adicionar-imagem", adicionar_imagem, name="adicionar_imagem"),
+    path("editar-imagem/<int:foto_id>", editar_imagem, name="editar_imagem"),
 ]
